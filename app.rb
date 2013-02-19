40da784575e493b1b143fe805661745fbb5ad373
Chris Smith <quartzmo@gmail.com>
2013-02-19 14:29:29 -0700

    mailchimp double_optin,send_welcome now true by default

--- app.rb
+++ app.rb
@@ -17,10 +17,15 @@ end
 post '/signup' do
   email = params[:email]
   unless email.nil? || email.strip.empty?
+
     mailchimp = Hominid::API.new(settings.mailchimp_api_key)
     list_id = mailchimp.find_list_id_by_name(settings.mailchimp_list_name)
     raise "Unable to retrieve list id from MailChimp API." unless list_id
-    mailchimp.list_subscribe(list_id, email, {}, 'html', false, true, true, false)
+
+    # http://apidocs.mailchimp.com/api/rtfm/listsubscribe.func.php
+    # double_optin, update_existing, replace_interests, send_welcome are all true by default (change as desired)
+    mailchimp.list_subscribe(list_id, email, {}, 'html', true, true, true, true)
+
   end
   "Success."
 end
